68a9b454c8d09679f5278b4b94e4ab28668919b0
Lê Anh Duy <duylase184704@fpt.edu.vn>
2026-07-07 17:13:58 +0700

    feat(shared): tạo GeoService hỗ trợ xử lý dữ liệu không gian PostGIS

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,10 +3,17 @@ import { ConfigService } from '@nestjs/config';
 import { SupabaseConfig } from './config/supabase.config';
 import { PrismaConfig } from './config/prisma.config';
 import { PayosConfig } from './config/payos.config';
+import { GeoService } from './geo/geo.service';
 
 @Global()
 @Module({
-    providers: [ConfigService, SupabaseConfig, PrismaConfig, PayosConfig],
-    exports: [SupabaseConfig, PrismaConfig, PayosConfig]
+  providers: [
+    ConfigService,
+    SupabaseConfig,
+    PrismaConfig,
+    PayosConfig,
+    GeoService,
+  ],
+  exports: [SupabaseConfig, PrismaConfig, PayosConfig, GeoService],
 })
-export class SharedModule { }
+export class SharedModule {}
