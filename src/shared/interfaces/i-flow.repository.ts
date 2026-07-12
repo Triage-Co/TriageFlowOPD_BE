dff20c4f8e89fbe79ca8cce6ae003f10979a62ef
ThaiNgocSE172515 <ngocdtse172515@fpt.edu.vn>
2026-07-12 23:40:52 +0700

    feat: add find all, find one flow

--- src/shared/interfaces/i-flow.repository.ts
+++ src/shared/interfaces/i-flow.repository.ts
@@ -1,4 +1,6 @@
 export interface IFlowRepository {
   findByStepId(account_id: string, id: string): Promise<any>;
-  findAll(account_id: string): Promise<any>;
+  findAllByAccountId(account_id: string): Promise<any>;
+  findAll(): Promise<any>;
+  findById(flow_id: string): Promise<any>;
 }
