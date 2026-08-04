f7e0808551ac8d36bec8d8eb14ef8a03f9f2ae9b
ThaiNgocSE172515 <ngocdtse172515@fpt.edu.vn>
2026-08-04 12:43:51 +0700

    feat: cap nhat service order

--- src/routes/service_order/service_order.module.ts
+++ src/routes/service_order/service_order.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ServiceOrderService } from './service_order.service';
 import { ServiceOrderController } from './service_order.controller';
+import { TransactionModule } from '../transaction/transaction.module';
 
 @Module({
+  imports: [TransactionModule],
   controllers: [ServiceOrderController],
   providers: [ServiceOrderService],
 })
